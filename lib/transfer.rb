15568572854d714ceb8fc1ad0d2d49c29df85110
marior9 <mariorodriguezan@gmail.com>
2019-04-04 18:10:53 -0400

    missing

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Transfer
   # your code here
+
+    attr_accessor :sender, :receiver, :status, :amount
+    def initialize(account1, account2, amount)
+      @sender= account1
+      @receiver= account2
+      @amount= amount
+      @status= 'pending'
+    end
+
+      def valid?
+        if self.sender.valid? && self.receiver.valid?
+            true
+        end
+      end
+
+      def execute_transaction
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount
+
+
+        # if sender_balance && receiver_balance
+        self.status= 'complete'
+        self.sender.balance
+        # end
+      end
+
 end
